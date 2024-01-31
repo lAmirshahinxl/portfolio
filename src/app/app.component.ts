56b081681e967b5db6e8aa1a012fe1aad02b23d7
lAmirshahinxl <lamirshahinxl@gmail.com>
2024-01-31 19:23:32 +0330

    import index page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IndexComponent } from './features/index/index.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, IndexComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
+
+  changeTitle(): void {
+    const possibleCharacters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const randomString = Array.from(
+      { length: 10 },
+      () =>
+        possibleCharacters[
+          Math.floor(Math.random() * possibleCharacters.length)
+        ]
+    ).join('');
+    this.title = randomString;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './features/index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent,
+    children: [
+      {
+        path: 'movies',
+        loadComponent: () =>
+          import('./features/index/index.component').then(
+            (m) => m.IndexComponent
+          ),
+      },
+    ],
+  },
+];
+
