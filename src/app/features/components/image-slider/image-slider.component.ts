2d07f42cac03d221b66564210de6cd2c950246ad
lAmirshahinxl <lamirshahinxl@gmail.com>
2024-02-15 19:17:43 +0330

    add projects

---
+++ src/app/features/components/image-slider/image-slider.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-slider',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './image-slider.component.html',
+  styleUrl: './image-slider.component.scss',
+})
+export class ImageSliderComponent {}
--- src/app/shared/utils/statics.ts
+++ src/app/shared/utils/statics.ts
@@ -4,30 +4,51 @@ import { ProjectModel } from '../models/project.model';
 export const PROJECTS_LIST: ProjectModel[] = [
   new ProjectModel(
     'Bitimen Crypto Currency Application',
-    'Application Deployment',
+    'Application Development',
     'Android & IOS Application Trade smarter, not harder. Bitimen puts the power of cryptocurrency in your pocket.',
-    'project2.png',
+    'bitimen.jpg',
     'https://bitimen.com/'
   ),
   new ProjectModel(
-    'Bitimen Crypto Currency Application',
-    'Application Deployment',
-    'Android & IOS Application Trade smarter, not harder. Bitimen puts the power of cryptocurrency in your pocket.',
-    'project2.png',
+    'Woodcomerce Shopping Application',
+    'Application Development',
+    'A world of wood at your fingertips: Explore endless possibilities with Wood-commerce',
+    'woodcomerce.jpg',
     'https://bitimen.com/'
   ),
   new ProjectModel(
-    'Bitimen Crypto Currency Application',
-    'Application Deployment',
-    'Android & IOS Application Trade smarter, not harder. Bitimen puts the power of cryptocurrency in your pocket.',
-    'project2.png',
+    'Aqila Crypto Currency Wallet',
+    'Application Development',
+    'The Aqila Crypto Currency Wallet is a software program designed to securely store private and public keys used for interacting with blockchain networks and managing various cryptocurrencies.',
+    'aqila.jpg',
     'https://bitimen.com/'
   ),
   new ProjectModel(
-    'Bitimen Crypto Currency Application',
-    'Application Deployment',
-    'Android & IOS Application Trade smarter, not harder. Bitimen puts the power of cryptocurrency in your pocket.',
-    'project2.png',
+    'Nobitex Site',
+    'Website Development',
+    ' Nobitex Explore Nobitex is the largest digital currency exchange in Iran, providing professional services for buying and selling digital    currencies without intermediaries',
+    'nobitex.jpg',
+    'https://bitimen.com/'
+  ),
+  new ProjectModel(
+    'Zaban Application',
+    'Application Development',
+    'Zaban is an AI-powered language learning app designed to tailor content to your interests, so you can stay engaged and motivated as you work towards fluency in Iran, Spanish, and more. Crafted to match your level, Zaban makes it easy to learn at your own pace and on your own terms',
+    'zaban-mobile.jpg',
+    'https://bitimen.com/'
+  ),
+  new ProjectModel(
+    'ZalTV Television Application',
+    'Application Development',
+    'ZalTV is a dedicated IPTV player designed for simplicity and speed. It caters to users who want a straightforward way to watch Internet Protocol Television (IPTV) content on their large screens and TV remote controls.',
+    'zaltv.jpg',
+    'https://bitimen.com/'
+  ),
+  new ProjectModel(
+    'Zaban WebSite',
+    'Website Development',
+    'Zaban is an AI-powered language learning app designed to tailor content to your interests, so you can stay engaged and motivated as you work towards fluency in Iran, Spanish, and more. Crafted to match your level, Zaban makes it easy to learn at your own pace and on your own terms',
+    'zaban-laptop.jpg',
     'https://bitimen.com/'
   ),
 ];
